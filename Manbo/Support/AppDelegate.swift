fd622b868ab8aa9616373abb4d334bbd11b4cbd6
yoogail105 <yoogail105@gmail.com>
2021-11-22 08:24:28 +0900

    Iteration1: UserData저장

--- Manbo/Support/AppDelegate.swift
+++ Manbo/Support/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().shadowImage = UIImage()
         UINavigationBar.appearance().tintColor = .black
         
+        IQKeyboardManager.shared.enable = true
           
         return true
     }
