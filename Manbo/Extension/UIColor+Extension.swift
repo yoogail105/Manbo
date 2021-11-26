5a1a76ce614283a09fa49abaea7c25c2fae3e03c
yoogail105 <yoogail105@gmail.com>
2021-11-26 20:52:10 +0900

    Error: calendarViewCell delegateㅠㅠ

--- Manbo/Extension/UIColor+Extension.swift
+++ Manbo/Extension/UIColor+Extension.swift
@@ -25,7 +25,7 @@ extension UIColor {
     
     convenience init(hex value: Int, alpha: CGFloat = 1.0) {
         self.init(
-            red: CGFloat((value & 0xFF0000) >> 16 ) / 255.0,
+        red: CGFloat((value & 0xFF0000) >> 16 ) / 255.0,
             green: CGFloat((value & 0x00FF00) >> 8 ) / 255.0,
             blue: CGFloat(value & 0x0000FF) / 255.0,
             alpha: alpha
