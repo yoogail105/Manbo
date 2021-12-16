688abfc1ac0806cc537cf867edd355afa15a3fdf
yoogail105 <yoogail105@gmail.com>
2021-12-16 18:19:52 +0900

    push Notification

--- Manbo/Support/AppDelegate.swift
+++ Manbo/Support/AppDelegate.swift
@@ -12,9 +12,9 @@ import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -27,37 +27,57 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // is now using a newer version.
         let config = Realm.Configuration(
             schemaVersion: 1,
-        // Use this configuration when opening realms
+            // Use this configuration when opening realms
             migrationBlock: { migration, oldSchemaVersion in
-        if (oldSchemaVersion < 1) {
-
-        }
-    })
+                if (oldSchemaVersion < 1) {
+                    
+                }
+            })
         // 새로운 설정을 기본 저장소에 적용
         Realm.Configuration.defaultConfiguration = config
         
         // firebase 초기화
         FirebaseApp.configure()
-       
-
+        
+        // 원격 알림 등록
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: { _, _ in }
+        )
+        
+        application.registerForRemoteNotifications()
+        Messaging.messaging().delegate = self
+        
+        // 현재 등록 토큰 가져오기
+        Messaging.messaging().token { token, error in
+          if let error = error {
+            print("Error fetching FCM registration token: \(error)")
+          } else if let token = token {
+            print("FCM registration token: \(token)")
+          }
+        }
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
 // MARK: - USER NOTIFICATION
@@ -65,12 +85,58 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 didReceive response: UNNotificationResponse,
                                 withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        print("사용자가 푸시를 클릭했습니다.")
+        
+        //userInfo: Key -1(광고), 2(채팅방), 3(사용자 설정)
+        print(response.notification.request.content.userInfo)
+        print(response.notification.request.content.body)
+        
+        // Key 1 파싱해보자
+        let userInfo = response.notification.request.content.userInfo
+        if userInfo[AnyHashable("Key")] as? Int == 1 {
+            print("광고 푸시 입니다.")
+        } else {
+            print("다른 푸시입니다.")
+        }
         completionHandler()
     }
-
+    
+    // 포그라운드 수신: Local Notification에서도 동일하게 동작한다!
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.alert, .badge, .sound])
+        if #available(iOS 14.0, *) {
+        completionHandler([.list, .banner, .badge, .sound])
+        } else {
+            completionHandler([.alert, .badge, .sound])
+        }
+            
     }
+    
+    
+    // Firebase 토큰과 애플의 토큰 매칭
+    func application(application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      Messaging.messaging().apnsToken = deviceToken
+    }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    // 토큰 갱신 모니터링
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+      print("Firebase registration token: \(String(describing: fcmToken))")
+
+      let dataDict: [String: String] = ["token": fcmToken ?? ""]
+      NotificationCenter.default.post(
+        name: Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
+      )
+      // TODO: If necessary send token to application server.
+      // Note: This callback is fired at each app startup and whenever a new token is generated.
+    }
+
+   
 }
